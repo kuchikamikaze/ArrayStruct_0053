fdec93665c406bd310a555ac1e18ac09e94eeb21
Naufal Arif <na713634@gmail.com>
2026-04-07 14:57:01 +0700

    input dan output data array tanpa inisialisasi

--- Array.cpp
+++ Array.cpp
@@ -16,4 +16,29 @@ int main () {
     // Cara menulis atau mengisi array
     dataku[2] = 600;
     cout << "Data ke - 3 yang baru : " << dataku[2] << endl;
+
+     // Mengisi array datamu satu per satu
+    cout << "Data Index ke - 0 : ";
+    cin >> datamu[0];
+
+    cout << "Data Index ke - 1 : ";
+    cin >> datamu[1];
+
+    cout << "Data Index ke - 2 : ";
+    cin >> datamu[2];
+
+    cout << "Data Index ke - 3 : ";
+    cin >> datamu[3];
+
+    cout << "Data Index ke - 4 : ";
+    cin >> datamu[4];
+
+    // Tampilkan array datamu satu per satu
+    cout << "//Tampilkan array datamu satu per satu" << endl;
+    cout << "Data ke - 1 : " << datamu[0] << endl;
+    cout << "Data ke - 2 : " << datamu[1] << endl;
+    cout << "Data ke - 3 : " << datamu[2] << endl;
+    cout << "Data ke - 4 : " << datamu[3] << endl;
+    cout << "Data ke - 5 : " << datamu[4] << endl;
+
 }
