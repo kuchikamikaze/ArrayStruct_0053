f9d74367f8f1768e7d0d28b1a3d1fcbdf7c49e49
Naufal Arif <na713634@gmail.com>
2026-04-07 15:13:52 +0700

    struct dalam struct

---
+++ StructStruct.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+    string desa;
+    string kota;
+};
+
+struct orang {
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
