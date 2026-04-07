66d4fc3f0f19596cd5ff9d1410704da908e19119
Naufal Arif <na713634@gmail.com>
2026-04-07 15:14:59 +0700

    inpur struct

--- StructStruct.cpp
+++ StructStruct.cpp
@@ -11,3 +11,23 @@ struct orang {
     DetailAlamat alamat;
     int umur;
 };
+
+int main() {
+    // Deklarasi obyek dari struct orang
+    orang mhs;
+
+    // Isi objek
+    cout << "Nama Mahasiswa: ";
+    cin >> mhs.nama;
+
+    cout << "Alamat Desa: ";
+    cin >> mhs.alamat.desa;
+
+    cout << "Alamat Kota: ";
+    cin >> mhs.alamat.kota;
+
+    cout << "Umur: ";
+    cin >> mhs.umur;
+
+    cout << endl;
+}
