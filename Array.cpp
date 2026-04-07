8ceaf97da1f2ec1fc43a381aafd62d24d5992136
Naufal Arif <na713634@gmail.com>
2026-04-07 14:54:21 +0700

    cara menampilkan dan merubah array

--- Array.cpp
+++ Array.cpp
@@ -7,4 +7,13 @@ int main () {
     //deklarasi tanpa inisialisasi
     int datamu[5];
     int x;
+    // Cara membaca array dataku
+    cout << "Data ke - 3 : " << dataku[2] << endl;
+
+    // Operasi Matematika
+    x = dataku[2] * 3;
+
+    // Cara menulis atau mengisi array
+    dataku[2] = 600;
+    cout << "Data ke - 3 yang baru : " << dataku[2] << endl;
 }
