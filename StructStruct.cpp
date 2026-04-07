98433f3d9c293766e907c97fc38a047140e80979
Naufal Arif <na713634@gmail.com>
2026-04-07 15:18:08 +0700

    output strct

--- StructStruct.cpp
+++ StructStruct.cpp
@@ -30,4 +30,11 @@ int main() {
     cin >> mhs.umur;
 
     cout << endl;
+    // Menampilkan Isi Obyek
+    cout << "Nama   : " << mhs.nama << endl;
+    cout << "Desa   : " << mhs.alamat.desa << endl;
+    cout << "Kota   : " << mhs.alamat.kota << endl;
+    cout << "Umur   : " << mhs.umur << endl;
+
+    return 0;
 }
